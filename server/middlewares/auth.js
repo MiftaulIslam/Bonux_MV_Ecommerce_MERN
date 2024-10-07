78c41d0b8f018a38fdd766a249aa419153ecccdb
Miftaul_Islam_Ariyan <124516497+MiftaulIslam@users.noreply.github.com>
2024-10-08 00:14:42 +0600

    bug fix

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,6 +4,7 @@ const catchAsync = require("./catchAsyncErrors");
 
 exports.isAuthenticated = catchAsync(async (req, res, next) => {
   const token = req.cookies['authenticate-token'];
+  console.log(token)
   if (!token) {
     return next(new ErrorHandler("Invalid token", 401));
   }
@@ -11,7 +12,7 @@ exports.isAuthenticated = catchAsync(async (req, res, next) => {
   try {
     const decoded = await jwtVerify(token);
     if (!decoded) {
-      return next(new ErrorHandler("Invalid token", 401));
+      return next(new ErrorHandler("Failed to load user", 401));
     }
     req.id = decoded.id;
     req.role = decoded.role;
