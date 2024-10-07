12a57422ba8a364afdc975420838fca478976350
Miftaul_Islam_Ariyan <124516497+MiftaulIslam@users.noreply.github.com>
2024-10-07 22:59:37 +0600

    Update SellerLayout.tsx

--- client/src/layouts/SellerLayout.tsx
+++ client/src/layouts/SellerLayout.tsx
@@ -11,7 +11,7 @@ import Close from "../widgets/icons/Close";
 import LogOutIcon from "../widgets/icons/LogoutIcon";
 import MenuIcon from "../widgets/icons/MenuIcon";
 import MessageSquareIcon from "../widgets/icons/MessageSquareIcon";
-import NotepadIcon from "../widgets/icons/NotepadIcon";
+import NotepadIcon from "../widgets/icons/NotepadIcon.tsx";
 import ProductIcon from "../widgets/icons/ProductIcon";
 import RightArrow from "../widgets/icons/RightArrow";
 import SellerCenterIcon from "../widgets/icons/SellerCenterIcon";
